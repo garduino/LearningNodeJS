39c1f3fd502bef26194239520d03ab9a8653e811
German Arduino <garduino@gmail.com>
2015-02-14 17:18:19 -0300

    First Commit

---
+++ program.js
@@ -0,0 +1,11 @@
+
+var total = Number(process.argv[2]) + Number(process.argv[3]) + Number(process.argv[4]);
+console.log(total);
+
+// Solución oficial
+// var result = 0
+    
+//    for (var i = 2; i < process.argv.length; i++)
+//      result += Number(process.argv[i])
+    
+//    console.log(result)
