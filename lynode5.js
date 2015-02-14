0acaa4d9d4000c5888f1303a83c87016a0e53ba2
German Arduino <garduino@gmail.com>
2015-02-14 18:47:49 -0300

    Filtered LS
    Exercises from Learnyounode (Nodeschool).

---
+++ lynode5.js
@@ -0,0 +1,25 @@
+// Solución alternativa
+// var fs = require('fs');
+
+// var dirname = process.argv[2];
+// var ext = process.argv[3];
+// var pat = RegExp('\\.' + ext + '$');
+
+// file = fs.readdir(dirname, function(err, files) {
+//  for (i = 0; i < files.length; i++) {
+//    if (pat.test(files[i])) {
+//      console.log(files[i]);
+//    }
+//  }
+// });
+
+// Solución oficial
+    var fs = require('fs')
+    var path = require('path')
+    
+    fs.readdir(process.argv[2], function (err, list) {
+      list.forEach(function (file) {
+        if (path.extname(file) === '.' + process.argv[3])
+          console.log(file)
+      })
+    })
